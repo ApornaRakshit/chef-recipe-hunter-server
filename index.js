5132461f4be6ef6151603646bc704eeac7dc40f0
APORNA16 <rakshit.aporna@gmail.com>
2023-05-03 12:25:23 +0600

    create 3 types of chef related data: chefs, chef, selected chef

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 const chefs = require('./data/chefs.json')
+const chef = require('./data/chef.json')
 
 app.use(cors())
 
@@ -13,6 +14,28 @@ app.get('/',(req,res)=>{
 
 app.get('/chefs',(req,res)=>{
     res.send(chefs)
+})
+
+app.get('/chef',(req,res)=>{
+    res.send(chef)
+})
+
+app.get('/chef/:id',(req, res) =>{
+    const id = req.params.id;
+    const selectedChef = chef.find(n=> n._id === id);
+    res.send(selectedChef)
+})
+
+app.get('/chefs/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(chef)
+    }
+    else{
+        const chefFood = chef.filter(n=> parseInt(n.chef_id) === id);
+        res.send(chefFood);
+    }
+    
 })
 
 app.listen(port,() =>{
