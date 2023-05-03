756b214da89f758c87e045c16a95e04373bb4b15
APORNA16 <rakshit.aporna@gmail.com>
2023-05-03 11:18:10 +0600

    chefs part reorganized

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const chefs = require('./data/chefs.json');
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000;
+const cors = require('cors')
+const port = process.env.PORT || 5000
 
-app.use(cors());
+const chefs = require('./data/chefs.json')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.send('Chef is cooking')
+    res.send('chef cooking is running')
 });
 
 app.get('/chefs',(req,res)=>{
     res.send(chefs)
 })
 
-app.listen(port,()=>{
-    console.log(`Chef cooking is running onnnn port: ${port}`)
+app.listen(port,() =>{
+    console.log(`chef cooking API is running on port:${port}`)
 })
+
+
 
